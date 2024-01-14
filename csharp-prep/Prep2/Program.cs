0b43ebd63c2621028036b7802a284aebce0dc035
Merrick Ward <war19027@byui.edu>
2024-01-13 23:09:08 -0700

    Projects 1 and 2 initial commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your score for the class? ");
+        string finalGrade = Console.ReadLine();
+
+        int finalScore = int.Parse(finalGrade);
+
+        int a = 90;
+        int b = 80;
+        int c = 70;
+        int d = 60;
+
+
+        if (finalScore >= a)
+        {
+            Console.WriteLine("Great job, your final grade is an A.");
+
+        }
+        else if (finalScore >= b)
+        {
+            Console.WriteLine("your grade is an B");
+
+        }
+        else if (finalScore >= c)
+        {
+            Console.WriteLine("your grade is an C");
+            
+        }
+        else if (finalScore >= d)
+        {
+            Console.WriteLine("your grade is an D");
+
+        }
+        else
+        {
+            Console.WriteLine("your grade is an F");
+        }
+
     }
 }
