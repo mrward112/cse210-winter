4894eb315f0226c0c2cb69c80a0061469d931cc3
Merrick Ward <war19027@byui.edu>
2024-01-20 16:24:58 -0700

    fulfilled core requirements for prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        //gets a random magic number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(0, 101);
+
+
+
+        //manually sets a magic number to play the guessing game with
+        // Console.Write("What is the magic number? ");
+        // string userMagicNumber = Console.ReadLine();
+        // int magicNumber = int.Parse(userMagicNumber);
+
+
+        //gets users guess for the magic number
+
+        int userGuess = 0;
+        do
+        {
+            Console.Write("What is your guess? ");
+            string userMagicNumberGuess = Console.ReadLine();
+            userGuess = int.Parse(userMagicNumberGuess);
+            if (userGuess > magicNumber)
+            {
+                Console.WriteLine("lower");
+            }
+
+            else if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+
+        } while (magicNumber != userGuess);
+
+
     }
 }
