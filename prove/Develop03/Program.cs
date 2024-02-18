87f163fdd02968cff991023fc3e6a37cc57c0c11
Merrick Ward <war19027@byui.edu>
2024-02-17 17:25:04 -0700

    update to scripture memorizor. Mostly functions

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -2,8 +2,46 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop03 World!");
-    }
+    public static void Main()
+        {
+            Console.WriteLine("Welcome to the Scripture Memorizer!");
+            // ******** gets the scripture Book, chapter, and verse from the user ********
+            Console.Write("Enter the name of the book: ");
+            string book = Console.ReadLine();
+
+            Console.Write("Enter the chapter number: ");
+            int chapter = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the verse number: ");
+            int verse = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a scripture verse here: ");
+            string text = Console.ReadLine();
+
+
+            Scripture scripture = new Scripture(book, chapter, verse, text);
+            Console.Clear();
+            // ******** gets the scripture Book, chapter, and verse from the user ********
+
+
+            scripture.Display(); 
+            Console.WriteLine("\n Here is your scripture. Press Enter to begin memorizing or type 'quit' to exit.");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "quit")
+                    break;
+
+                if (!scripture.HideRandomWord())
+                {
+                    Console.WriteLine("\nYou've memorized the scripture!!");
+                    break;
+                }
+
+                Console.Clear();
+                scripture.Display();
+                Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+            }
+        }
 }
