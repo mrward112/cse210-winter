4d86ecde8edf73ffac0c08e2cb02ab9e8ffbd32d
Merrick Ward <war19027@byui.edu>
2024-03-02 20:59:52 -0700

    Mindfulness commit

---
+++ example.cs
@@ -0,0 +1,16 @@
+Public class SecureConnection {
+	Public SecureConnection()
+    {
+        // add a constructor to start/initualize code
+    }
+
+    private void SendData()
+    {
+        // code that sends data to the server using necessary components
+    }
+
+    private void ReceiveData()
+    {
+        // logic that sends data back to the user using necessary components
+    }
+}
